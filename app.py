e8f5db455c6180012e18eb121bb047f55ccbcf3a
daniloui001 <daniel.louie@stonybrook.edu>
2023-12-14 21:10:26 -0500

    update

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ import os
 import sys
 import pandas as pd
 
+from flask import Flask, render_template, jsonify, request
+import requests
+
 app = Flask(__name__)
+
+API_KEY = os.getenv('API_KEY')
+
+PLACES_API_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
 
 @app.route('/')
 def index():
@@ -14,11 +22,54 @@ def index():
 
 @app.route('/newgame')
 def about():
-    return render_template('about.html')
+    return render_template('newgame.html')
 
 @app.route('/data')
 def data():
     return render_template('data.html')
+
+@app.route('/api/nearby-locations', methods=['GET'])
+def get_nearby_locations():
+    category = request.args.get('category')
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+
+    if not category or not lat or not lng:
+        return jsonify({'error': 'Missing required parameters'}), 400
+
+    if category == 'arcades':
+        place_type = 'arcade'
+    elif category == 'cafes':
+        place_type = 'cafe'
+    elif category == 'libraries':
+        place_type = 'library'
+    else:
+        return jsonify({'error': 'Invalid category'}), 400
+
+    params = {
+        'location': f'{lat},{lng}',
+        'radius': 1000, 
+        'type': place_type,
+        'key': API_KEY,
+    }
+
+    response = requests.get(PLACES_API_URL, params=params)
+
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to retrieve nearby locations'}), 500
+
+    data = response.json()
+
+    locations = []
+    for result in data.get('results', []):
+        locations.append({
+            'name': result.get('name'),
+            'address': result.get('vicinity'),
+            'latitude': result['geometry']['location']['lat'],
+            'longitude': result['geometry']['location']['lng'],
+        })
+
+    return jsonify({'locations': locations})
 
 if __name__ == '__main__':
     app.run(
